66f27ca60fd744078a850a754b7658de5fcdfcd8
STEPHEN FLYNN <stephenjflynn1@gmail.com>
2016-03-17 23:16:41 -0400

    sending predetermined text, need to send variables still

--- server.js
+++ server.js
@@ -1,26 +1,76 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var javascripts = '/public/javascripts/'
+var cors = require("cors");
+var javascripts = '/public/javascripts/';
+$ = require('jQuery');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+
+var formData = [{
+	 name: "bert muppet",
+	 email: "ernieismybud@gmail.com"
+	}
+	];
+
+
+
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/client/views/index.html');
+
+
+app.use(function(req,res,next){
+	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+	console.log(`'${req.url}'`);
+	next();
 });
+
+
+app.use(express.static("./public"));
+
+app.use(cors());
+
+  
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + '/client/views/index.html');
+	
+
+
+
+});
+
+app.get("/bunki-input", function(req, res) {
+	
+	res.sendFile(__dirname + '/client/views/bunki-input.html')
+	//formData.push(req.body);
+	//res.json(formData)
+});
+
+
+app.post("/bunki-input", function(req, res) {
+//formData.push(req.body);
+//document.write(insertInfo);
+
+});
+
 
 app.get(javascripts + 'response.js', function(req, res) {
 	res.sendFile(__dirname + javascripts + 'response.js');
 });
 
-app.post('/roomi-input', function(req, res) {
-	var name = req.query.name;
-	console.log(name);
-	res.send(name);
-	//res.sendFile(__dirname + '/client/views/response.html');
-});
+
 
 app.listen('3000', function() {
 	console.log("Listening on Local Host 3000...")
 });
+
+
+
+
+
+
+
+
+
